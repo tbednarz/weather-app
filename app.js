e7b03524f9a16327daf7df601880c5e27d2891f4
Master Tres <tresbednarz@gmail.com>
2019-06-13 18:50:19 -0400

    added error handling

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const request = require("request");
 const chalk = require("chalk");
 
-// const url =
-//   "https://api.darksky.net/forecast/75eba5b5a9e416e571688bc0e937d5a5/37.8267,-122?units=us&lang=en";
+const url =
+  "https://api.darksky.net/forecast/75eba5b5a9e416e571688bc0e937d5a5/37.8267,-122?units=us&lang=en";
 
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log("Unable to connect to weather service!");
-//   } else if (response.body.error) {
-//     console.log("Unable to find location");
-//   } else {
-//     const currentTemp = response.body.currently.temperature;
-//     const currentPrecip = response.body.currently.precipProbability;
-//     console.log(
-//       response.body.daily.data[0].summary +
-//         " It is currently " +
-//         currentTemp +
-//         " degrees out. " +
-//         "There is a " +
-//         currentPrecip +
-//         "% chance of rain."
-//     );
-//   }
-// });
+request({ url: url, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to weather service!");
+  } else if (response.body.error) {
+    console.log("Unable to find location");
+  } else {
+    const currentTemp = response.body.currently.temperature;
+    const currentPrecip = response.body.currently.precipProbability;
+    console.log(
+      response.body.daily.data[0].summary +
+        " It is currently " +
+        currentTemp +
+        " degrees out. " +
+        "There is a " +
+        currentPrecip +
+        "% chance of rain."
+    );
+  }
+});
 
-// Geocoding
+Geocoding
 
 
 const geoCodeUrl =
